c99b3ac8cab8941e40984b33c379bc1501049399
Abigail Ryan <abi.ryan91@gmail.com>
2024-09-20 16:24:22 +0000

    Add filter to TreatmenstList view to only show published treatments

--- treatments/views.py
+++ treatments/views.py
@@ -4,5 +4,5 @@ from .models import Treatment
 
 # Create your views here.
 class TreatmentList(generic.ListView):
-    queryset = Treatment.objects.all()
+    queryset = Treatment.objects.filter(status=1)
     template_name = "treatment_list.html"
