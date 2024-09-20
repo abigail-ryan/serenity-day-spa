c252c19c66ae9b5a4469d370e5328b5432f830f2
Abigail Ryan <abi.ryan91@gmail.com>
2024-09-20 16:47:54 +0000

    Add whitenoise and summernote installation packages

--- serenity_day_spa/urls.py
+++ serenity_day_spa/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("treatments.urls"), name='treatments-urls'),
     path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
+    path("", include("treatments.urls"), name='treatments-urls'),
 ]
--- treatments/admin.py
+++ treatments/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from .models import Treatment
+from django_summernote.admin import SummernoteModelAdmin
+
+@admin.register(Treatment)
+class TreatmentAdmin(SummernoteModelAdmin):
+
+    list_display = ('name', 'slug', 'status')
+    search_fields = ['name',]
+    list_filter = ('status',)
+    prepopulated_fields = {'slug': ('name',)}
+    summernote_fields = ('description', 'excerpt', 'price', 'duration', 'has_requirements', 'requirements_details',)
+
 
 # Register your models here.
-admin.site.register(Treatment)
+
