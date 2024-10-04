c7fb3c93a3671fcd685f385e91ac39e8224f2779
Abigail Ryan <abi.ryan91@gmail.com>
2024-10-04 22:25:24 +0000

    Update user account view and html page to show user their bookings

--- booking/forms.py
+++
@@ -1,8 +0,0 @@
-from .models import Appointment
-from django import forms
-
-
-class BookingForm(forms.ModelForm):
-    class Meta:
-        model = Appointment
-        fields = ('first name', 'last name', 'email', 'phone number', 'treatment', 'day', 'time', 'notes')
--- user_account/views.py
+++ user_account/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from django.http import HttpResponse
+from booking.models import Appointment
 
 # Create your views here.
 class UserAccount(TemplateView):
     template_name = 'my_account/my_account.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = self.request.user
+        context['appointments'] = Appointment.objects.filter(user=user)
+        
+        return context
