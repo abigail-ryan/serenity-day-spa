8770353042beaf345ff2bb60a3240f0ae20caa07
Abigail Ryan <abi.ryan91@gmail.com>
2024-10-06 19:16:56 +0000

    Fix broken urls and update bookinf forms.py from standard form to ModelForm

--- booking/forms.py
+++ booking/forms.py
@@ -1,12 +1,15 @@
 from django import forms
 from .models import Appointment, Treatment, TIME_SLOTS
 
-class BookingForm(forms.Form):
-    treatment = forms.ModelChoiceField(queryset=Treatment.objects.all())
-    day = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
-    time = forms.ChoiceField(choices=TIME_SLOTS)
-    first_name = forms.CharField(max_length=100)
-    last_name = forms.CharField(max_length=100)
-    email = forms.EmailField()
-    phone_number = forms.CharField(max_length=15)
-    notes = forms.CharField(required=False, widget=forms.Textarea)
+class BookingForm(forms.ModelForm):
+    class Meta:
+        model = Appointment
+        fields = ['treatment', 'day', 'time', 'first_name', 'last_name', 'email', 'phone_number', 'notes']
+        treatment = forms.ModelChoiceField(queryset=Treatment.objects.all())
+        day = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
+        time = forms.ChoiceField(choices=TIME_SLOTS)
+        first_name = forms.CharField(max_length=100)
+        last_name = forms.CharField(max_length=100)
+        email = forms.EmailField()
+        phone_number = forms.CharField(max_length=15)
+        notes = forms.CharField(required=False, widget=forms.Textarea)
--- booking/urls.py
+++ booking/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('booking', views.BookingView.as_view(), name='booking'),
-    path('booking/edit/int:pk>/', views.BookingEdit.as_view(), name='booking_edit'),
-    path('booking/delete/<int:pk>/', views.DeleteBooking.as_view(), name='booking_delete'),
+    path('booking/edit/<int:pk>/', views.BookingEdit.as_view(), name='booking-edit'),
+    path('booking/delete/<int:pk>/', views.DeleteBooking.as_view(), name='booking-delete'),
 ]
